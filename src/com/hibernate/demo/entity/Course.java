3d1f38782d0df4f2fc242fac823f0c8c6095520d
Piyush Kumar <piyush-kumar.piyush@dxc.com>
2020-04-07 23:57:55 +0530

    adding course entity

--- src/com/hibernate/demo/entity/Course.java
+++ src/com/hibernate/demo/entity/Course.java
@@ -1,10 +1,13 @@
 package com.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +22,9 @@ public class Course {
 	@Column(name="title")
 	private String title;
 	
+	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE
+							,CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 
 	public Course() {
